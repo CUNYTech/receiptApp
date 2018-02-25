5ea2b732371f42695ef46c230f4bce76514bb3df
Frank Orefice <Frankorefice1997@yahoo.com>
2018-02-25 15:09:28 -0500

    changed LoginResult class to Result class

--- src/com/groupmeal/farsh/AppDB.java
+++ src/com/groupmeal/farsh/AppDB.java
@@ -1,5 +1,7 @@
 package com.groupmeal.farsh;
 
 public class AppDB {
+	private User currentUser;
 	
+	public createAccount(String fn, String ln, String em, String ps)
 }
---
+++ src/com/groupmeal/farsh/Result.java
@@ -0,0 +1,12 @@
+package com.groupmeal.farsh;
+
+public class Result {
+	public String error;
+	public int errorCode;
+	public JSONObject result;
+	
+	public Result(String error, String errorCode)
+	{
+		
+	}
+}
